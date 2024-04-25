3c525a32b9b5b4b2f3a6c0744dd5024083850161
Rodrigo Lima <rodrigolima@brasilcard.net>
2024-04-24 22:02:19 -0300

    feat: improvments on header menus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,31 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
+import { Image } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-
+    library.add(fas);
     const { logout, cookies } = useAuth();
 
-    const handleSubmit = async (e) => {
+    const handleLogout = async (e) => {
         e.preventDefault();
         logout(true);
     }
+
+    const user = cookies.user;
+
+    const UserMenu = (
+        <Image
+            src={user.image ? process.env.REACT_APP_API_BASE_URL + 'storage/' + user.image : 'https://via.placeholder.com/150'}
+            alt="UserName profile image"
+            roundedCircle
+            style={{ width: '30px' }}
+        />
+    )
 
     return (
         <header className="App-header">
@@ -24,22 +39,30 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="justify-content-end flex-grow-1 pe-3">
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="/home">Home</Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
+                            {cookies.token ? (
+                                <>
+                                    <NavDropdown title="Relatórios" id="reports-dropdown">
+                                        <NavDropdown.Item href="/water-intake-report">Consumo de Água</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                    </NavDropdown>
+                                    <NavDropdown title={UserMenu} id="profile-dropdown">
+                                        <NavDropdown.Item href="/settings">
+                                            <FontAwesomeIcon icon={['fa', 'sliders']} /> Preferências
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={handleLogout} style={{ 'color': 'red' }}>
+                                            <FontAwesomeIcon icon={['fa', 'right-from-bracket']} /> Logout
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </>
+                            ) : null}
                         </Nav>
                     </Navbar.Collapse>
-                    {cookies.token ? (<Button variant="primary" className="d-flex" onClick={handleSubmit}>Logout</Button>) : null}
                 </Container>
             </Navbar>
         </header>
